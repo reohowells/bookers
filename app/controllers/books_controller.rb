9bc2f35037d0f2eb65733cadf24e553e5b23b033
howells reo <reo.howells@gmail.com>
2020-08-03 22:30:49 +0900

    再提出

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -17,20 +17,30 @@ class BooksController < ApplicationController
   end
 
   def update
-  	book = Book.find(params[:id])
-	book.update(book_params)
-  	redirect_to book_path(book)
+  	@book = Book.find(params[:id])
+	if @book.update(book_params)
+  	redirect_to book_path(@book), notice: "successfully"
+  else
+      render "edit"
+  end
   end
 	def destroy
 		book = Book.find(params[:id])
 		book.destroy
 		redirect_to books_path
 	end
+  
   def create
-  	book = Book.new(book_params)
-  	book.save
-  	redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book), notice: "successfully"
+    else
+      @books = Book.all
+      render "index"
+
   end
+  end
+
 
 private
 def book_params
